e06eb20ed5222f6d5a5fe65f48bf07aba64ba8f0
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2022-06-06 16:03:53 -0400

    edit

--- src/ICS4UProject/Body.java
+++ src/ICS4UProject/Body.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 /**
  *
  */
-public interface Body extends Kinetic{
+public interface Body extends KineticsWithSize{
     public Vector getGravity();
     public Vector getDrag();
     public double getDragCoe();
