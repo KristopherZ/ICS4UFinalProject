ecaf5c4232a82a117ec57ac5dd251e7d49df2e10
javid <javidryan46@gmail.com>
2022-06-09 12:41:45 -0400

    commented body class

--- src/ICS4UProject/Body.java
+++ src/ICS4UProject/Body.java
@@ -3,17 +3,21 @@ package ICS4UProject;
 import java.util.ArrayList;
 
 /**
- *
+ * Body interface
+ * Provides appropriate getting and setter methods for key player components
  */
 
 public interface Body extends KineticsWithSize{
+    /**
+     * Getter and setter for gravity, drag, drag coefficient normal friction and friction
+     */
     public Vector getGravity();
+    public void setGravity(Vector v);
     public Vector getDrag();
+    public void setDragCoe(double dragCoe);
     public double getDragCoe();
     public Vector getNormalForce();
-    public Vector getFriction();
-    public void setGravity(Vector v);
-    public void setDragCoe(double dragCoe);
     public void setNormalForce(Vector v);
+    public Vector getFriction();
     public void setFriction(Vector v);
 }
