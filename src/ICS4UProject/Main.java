a7fdd56703ca5c78f0a6ce711808fa7d92d0fdde
javid <javidryan46@gmail.com>
2022-06-18 15:42:12 -0400

    not finished player

--- src/ICS4UProject/Main.java
+++ src/ICS4UProject/Main.java
@@ -26,24 +26,42 @@ public class Main extends Application{
         Platform pt2 = new Platform(900,0,100,500);
         Platform pt3 = new Platform(100,400,800,100);
         Platform pt4 = new Platform(700,300,400,400);
-        File file = new File("image.jpg");
+        File file = new File("playerSprite.png");
+        File playerSprite = new File("image.jpg");
+
         Image image = new Image(file.toURI().toURL().toString(), false);
+        Image playerImage = new Image(playerSprite.toURI().toURL().toString(), false);
 //        Enemy enemy = new Enemy(500,0,50,50,image);
         final EnemyShell[] enemy = {new EnemyShell(500, 0, 50, 50, image)};
         pt1.addKinetic(enemy[0]);
         pt2.addKinetic(enemy[0]);
         pt3.addKinetic(enemy[0]);
         pt4.addKinetic(enemy[0]);
+
         enemy[0].getPlatformList().add(pt1);
         enemy[0].getPlatformList().add(pt2);
         enemy[0].getPlatformList().add(pt3);
         enemy[0].getPlatformList().add(pt4);
         enemy[0].setGravity(new Vector(0,2000));
         enemy[0].setVelocity(new Vector(500,0));
+//        Player player = new Player(500,0,50,50,playerImage,k);
         Group root = new Group(pt1.getRectangle(),pt2.getRectangle(),pt3.getRectangle(),pt4.getRectangle(), enemy[0].getImage());
         primaryStage.setWidth(1000);
         primaryStage.setHeight(500);
         Scene scene = new Scene(root);
+        KeyInput k = new KeyInput(scene);
+
+        Player player = new Player(500,40,50,50,playerImage,k);
+        root.getChildren().add(player.getImage());
+
+        // PLAYER
+        pt1.addKinetic(player);
+        pt2.addKinetic(player);
+        pt3.addKinetic(player);
+        pt4.addKinetic(player);
+
+        player.setPhysics();
+
         primaryStage.setScene(scene);
         AnimationTimer t = new AnimationTimer() {
             @Override
@@ -51,6 +69,7 @@ public class Main extends Application{
                 if(lastUpdatedTime > 0){
                     long elapsedTime = timestamp - lastUpdatedTime;
                     enemy[0].update(elapsedTime);
+                    player.update(elapsedTime);
                     if(enemy[0].hit)  {
                         enemy[0].close();
                     }
@@ -58,7 +77,6 @@ public class Main extends Application{
                     pt2.update(elapsedTime);
                     pt3.update(elapsedTime);
                     pt4.update(elapsedTime);
-
                 }
                 lastUpdatedTime = timestamp;
             }
--- src/ICS4UProject/Player.java
+++ src/ICS4UProject/Player.java
@@ -1,82 +1,63 @@
-//package ICS4UProject;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//
-//public class Player extends CollisionBodyRec {
-//    private final static double coefficientOfZerothTerm = 100, coefficientOfFirstTerm = 50, exponent = 1.7;
-//    private ArrayList<Body> kineticList = new ArrayList<>();
-//    private ArrayList<Vector> normalForceList = new ArrayList<>();
-//    private boolean isUpdate = true;
-//
-//    public InteractiveObjectRec(double x, double y, double sizeX, double sizeY) {
-//        super(x, y, sizeX, sizeY);
-//    }
-//
-//    public void addKinetic(Body k) {
-//        kineticList.add(k);
-//        Vector normalForce = new Vector();
-//        normalForceList.add(normalForce);
-//        k.getForceList().add(normalForce);
-//    }
-//
-//    public void collide() {
-//
-//        for (int i = 0; i < kineticList.size(); i++) {
-//            CollisionEvent e = collideWith(kineticList.get(i));
-////            System.out.println(Arrays.toString(e.getDepth()));
-//            double min = Double.POSITIVE_INFINITY;
-//            int minIndex = -1;
-//            for (int j = 0; j < 4; j++) {
-//                if (e.getDepth()[j] < min && e.getDepth()[j] > 0) {
-//                    min = e.getDepth()[j];
-//                    minIndex = j;
-//                }
-//            }
-//            System.out.println(minIndex);
-//            switch (minIndex) {
-//                case 0:
-//                    normalForceList.get(i).set(
-//                            new Vector(0,
-//                                    (-Math.pow(e.getDepth()[minIndex], exponent) * coefficientOfZerothTerm
-//                                            - getElasticity()[0] * kineticList.get(i).getElasticity()[1] * (kineticList.get(i).getVelocity().getY() - getVelocity().getY()) * coefficientOfFirstTerm) * kineticList.get(i).getMass()));
-//                    break;
-//                case 1:
-//                    normalForceList.get(i).set(
-//                            new Vector(0,
-//                                    (Math.pow(e.getDepth()[minIndex], exponent) * coefficientOfZerothTerm
-//                                            - getElasticity()[1] * kineticList.get(i).getElasticity()[0] * (kineticList.get(i).getVelocity().getY() - getVelocity().getY()) * coefficientOfFirstTerm) * kineticList.get(i).getMass()));
-//                    break;
-//                case 2:
-//                    normalForceList.get(i).set(
-//                            new Vector((-Math.pow(e.getDepth()[minIndex], exponent) * coefficientOfZerothTerm
-//                                    - getElasticity()[2] * kineticList.get(i).getElasticity()[3] * (kineticList.get(i).getVelocity().getX() - getVelocity().getX()) * coefficientOfFirstTerm) * kineticList.get(i).getMass(),
-//                                    0));
-//                    break;
-//                case 3:
-//                    normalForceList.get(i).set(
-//                            new Vector((Math.pow(e.getDepth()[minIndex], exponent) * coefficientOfZerothTerm
-//                                    - getElasticity()[3] * kineticList.get(i).getElasticity()[2] * (kineticList.get(i).getVelocity().getX() - getVelocity().getX()) * coefficientOfFirstTerm) * kineticList.get(i).getMass(),
-//                                    0));
-//                    break;
-//                default:
-//                    normalForceList.get(i).set(new Vector());
-//                    break;
-//            }
-//        }
-//    }
-//
-//    @Override
-//    public void update(long elapsedTime) {
-//        if(isUpdate){
-//            super.update(elapsedTime);
-//            collide();
-//        }
-//    }
-//
-//    @Override
-//    public void close() {
-//        super.close();
-//        isUpdate = false;
-//    }
-//}
+package ICS4UProject;
+
+import java.awt.*;
+
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Player extends CollisionBodyImage {
+
+    private boolean isUpdate = true;
+    private final static double coefficientOfZerothTerm = 100, coefficientOfFirstTerm = 50, exponent = 1.7;
+    private final int[] elasticity = {1,1,1,1};
+    private ArrayList<Body> kineticList = new ArrayList<>();
+    private ArrayList<Vector> normalForceList = new ArrayList<>();
+    private ArrayList<Platform> platformList = new ArrayList<>();
+    private ArrayList<Platform> getPlatformList() {
+        return platformList;
+    }
+
+    KeyInput k;
+
+    public Player(double x, double y, double sizeX, double sizeY, Image image, KeyInput k) {
+        super(x, y, sizeX, sizeY, image);
+        this.k = k;
+    }
+
+    public void setPhysics() {
+        this.setGravity(new Vector(0,1000));
+        this.setVelocity(new Vector(0,0));
+    }
+
+
+    private void keyMovement() {
+        if(k.iswPressed()){
+            this.setAppliedForce(new Vector(0,-3000));
+        }else if(k.isdPressed()){
+            this.setAppliedForce(new Vector(3000,0));
+        }else if(k.isaPressed()){
+            this.setAppliedForce(new Vector(-3000,0));
+        }else{
+            this.setAppliedForce(new Vector(0,0));
+        }
+    }
+
+    @Override
+    public void update(long elapsedTime) {
+        if(isUpdate){
+            super.update(elapsedTime);
+            keyMovement();
+        }
+    }
+
+    @Override
+    public void close() {
+        super.close();
+        isUpdate = false;
+    }
+
+}
