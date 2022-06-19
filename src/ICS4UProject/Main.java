1a3e912699ba61623e16460dfd6ddf6d70e17fd3
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2022-06-18 20:25:44 -0400

    menu bar

--- src/ICS4UProject/Main.java
+++ src/ICS4UProject/Main.java
@@ -6,8 +6,13 @@ import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -22,7 +27,19 @@ public class Main extends Application{
     public void start(Stage stage) throws Exception {
         stage.setWidth(500);
         stage.setHeight(500);
-        Group group = new Group();
+        Menu menu1 = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction((e)->{
+            System.exit(0);
+        });
+        menu1.getItems().add(exit);
+        MenuBar mb = new MenuBar(menu1);
+        mb.prefWidthProperty().bind(stage.widthProperty());
+        Group group = new Group(mb);
+
+        group.minHeight(500);
+        group.minHeight(500);
+
         Scene scene = new Scene(group);
         KeyInput k = new KeyInput(scene);
         stage.setScene(scene);
