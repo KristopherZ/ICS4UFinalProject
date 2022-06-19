62353dde3170a9fa3ce4ac0c7277165e8f1e02a2
javid <javidryan46@gmail.com>
2022-06-19 03:13:06 -0400

    - player is able to jump and kill enemy - comments added to player, enemy, and enemy shell - velocity cap added to player to improve movement - still need to fix enemy harming player

--- src/ICS4UProject/Enemy.java
+++ src/ICS4UProject/Enemy.java
@@ -4,31 +4,77 @@ import javafx.scene.image.Image;
 
 import java.util.ArrayList;
 
+/**
+ * represents the simple enemy object
+ * better known as the "goomba"
+ */
+
 public class Enemy extends CollisionBodyImage {
 
     private final static double coefficientOfZerothTerm = 100, coefficientOfFirstTerm = 50, exponent= 1.7;
+    private final ArrayList<Player> players = new ArrayList<>();
+    private final ArrayList<PlatformImage> platformImageList = new ArrayList<>();
 
-    private ArrayList<PlatformImage> platformImageList = new ArrayList<>();
-
+    /**
+     * To construct a player
+     * @param x the x coordinate of the enemy
+     * @param y the y coordinate of the enemy
+     * @param sizeX the width
+     * @param sizeY the height
+     * @param image the image of the enemy
+     */
     public Enemy(double x, double y, double sizeX, double sizeY, Image image) {
         super(x, y, sizeX, sizeY, image);
         setElasticity(new double[]{1,1,1,1});
     }
 
+    /**
+     * To get the current platformImage list
+     * @return the current platformImage list
+     */
     public ArrayList<PlatformImage> getPlatformImageList() {
         return platformImageList;
     }
 
+    /**
+     * To add to the list of players
+     * @param player the player
+     */
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    /**
+     * exert forces in the opposite direction of impact to make the enemy go back and forth
+     * closes the player if it touches the enemy
+     */
+    private void collide() {
+        for(PlatformImage i : platformImageList) {
+            if(i.collideWith(this).getCollisionPosition()[2]){
+                setVelocity(new Vector(-100,0));
+            }else if(i.collideWith(this).getCollisionPosition()[3]){
+                setVelocity(new Vector(100,0));
+            }
+            for(Player j : players) {
+                if(j.jumpOnEnemy(this)) {
+                    this.close();
+                }
+            }
+
+        }
+    }
+
+    /**
+     * updates collision position of the enemy
+     * @param elapsedTime the time between two update is called
+     */
     @Override
     public void update(long elapsedTime) {
         super.update(elapsedTime);
-//        for(Platform i : platformList) {
-//            if(i.collideWith(this).getCollisionPosition()[2]){
-//                setVelocity(new Vector(-500,0));
-//            }else if(i.collideWith(this).getCollisionPosition()[3]){
-//                setVelocity(new Vector(500,0));
-//            }
-//        }
+        for(Player i : players) {
+            this.collideWith(i);
+        }
+        collide();
     }
 
 }
--- src/ICS4UProject/Player.java
+++ src/ICS4UProject/Player.java
@@ -4,31 +4,45 @@ import javafx.scene.image.Image;
 
 import java.util.ArrayList;
 
+/**
+ * represents the main player object
+ */
+
 public class Player extends CollisionBodyImage {
 
-    private boolean isUpdate = true;
-
-    private Vector horizontalForce = new Vector();
-
-    private ArrayList<PlatformImage> PlatformImageList = new ArrayList<>();
-
+    private final Vector HORIZONTAL_FORCE = new Vector();
+    private final ArrayList<PlatformImage> PLATFORM_IMAGE_LIST = new ArrayList<>();
+    private final KeyInput k;
     public ArrayList<PlatformImage> getPlatformImageList() {
-        return PlatformImageList;
+        return PLATFORM_IMAGE_LIST;
     }
     private Image[] playerStates = new Image[5];
+    private boolean isUpdate = true;
 
-    KeyInput k;
-
+    /**
+     * To construct a player
+     * @param x the x coordinate of the player
+     * @param y the y coordinate of the player
+     * @param sizeX the width
+     * @param sizeY the height
+     * @param image the image of the platform
+     * @param k the key detection
+     */
     public Player(double x, double y, double sizeX, double sizeY, Image image, KeyInput k) {
         super(x, y, sizeX, sizeY, image);
-        getForceList().add(horizontalForce);
+        getForceList().add(HORIZONTAL_FORCE);
         setElasticity(new double[]{1,1,1,1});
         playerStates = new Image[]{image,image,image,image,image};
         this.k = k;
     }
 
+    /**
+     * Checks if the player is on the ground
+     * Useful to avoid the player from "flying" and jumping while in the air
+     * @return if the player is touching the ground
+     */
     private boolean touchingGround() {
-        for(PlatformImage i : PlatformImageList) {
+        for(PlatformImage i : PLATFORM_IMAGE_LIST) {
             if(i.collideWith(this).getCollisionPosition()[0]) {
                 return true;
             }
@@ -36,44 +50,81 @@ public class Player extends CollisionBodyImage {
         return false;
     }
 
+    /**
+     * Checks if the player has jumped on the enemy
+     * @param enemy current enemy
+     * @return if the player is jumping on the enemy
+     */
+    public boolean jumpOnEnemy(Enemy enemy) {
+        return this.collideWith(enemy).getCollisionPosition()[1];
+    }
 
+//    public boolean killPlayer(Enemy enemy) {
+//        return this.collideWith(enemy).getCollisionPosition()[2] || this.collideWith(enemy).getCollisionPosition()[3];
+//    }
+
+    /**
+     * exert a forces relative to the respective key press for player movement
+     * w - up
+     * a - left
+     * d - right
+     */
     private void keyMovement() {
         if(k.isaPressed()&&k.isdPressed()){
-            horizontalForce.set(new Vector());
+            HORIZONTAL_FORCE.set(new Vector());
         }else if(k.isaPressed()) {
             if(touchingGround())
-                horizontalForce.set(new Vector(-4000, 0));
+                HORIZONTAL_FORCE.set(new Vector(-4000, 0));
             else
-                horizontalForce.set(new Vector(-600, 0));
+                HORIZONTAL_FORCE.set(new Vector(-600, 0));
         }else if(k.isdPressed()){
             if(touchingGround())
-                horizontalForce.set(new Vector(4000, 0));
+                HORIZONTAL_FORCE.set(new Vector(4000, 0));
             else
-                horizontalForce.set(new Vector(600, 0));
+                HORIZONTAL_FORCE.set(new Vector(600, 0));
         }else{
-            horizontalForce.set(new Vector());
+            HORIZONTAL_FORCE.set(new Vector());
         }
         if(k.iswPressed() && touchingGround()){
             this.setAppliedForce(new Vector(0,-11000),150);
         }
     }
 
+    /**
+     * Sets the player image depending on current movement, to simulate animation
+     */
+    private void playerStateChange() {
+        if(getVelocity().getY() < 0)
+            getImage().setImage(playerStates[0]);
+        else if(getVelocity().getX() > 0)
+            getImage().setImage(playerStates[1]);
+        else if(getVelocity().getX() < 0)
+            getImage().setImage(playerStates[2]);
+        else
+            getImage().setImage(playerStates[3]);
+    }
+
+    /**
+     * includes a horizontal velocity cap to avoid player acceleration beyond a certain point
+     * @param elapsedTime the time between two update is called
+     */
     @Override
     public void update(long elapsedTime) {
         if(isUpdate){
             super.update(elapsedTime);
             keyMovement();
-            if(getVelocity().getY() < 0)
-                getImage().setImage(playerStates[0]);
-            else if(getVelocity().getX() > 0)
-                getImage().setImage(playerStates[1]);
-            else if(getVelocity().getX() < 0)
-                getImage().setImage(playerStates[2]);
-            else
-                getImage().setImage(playerStates[3]);
+            playerStateChange();
+            if(getVelocity().getX() > 500)
+                setVelocity(new Vector(500, getVelocity().getY()));
+            if(getVelocity().getX() < -500) {
+                setVelocity(new Vector(-500,getVelocity().getY()));
+            }
         }
     }
 
+    /**
+     * Closes the player object by stopping it from updating
+     */
     @Override
     public void close() {
         super.close();
