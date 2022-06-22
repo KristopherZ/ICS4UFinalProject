8a1a3771f688aa86c91c4c29cdd59fc8e7c1da6c
KristopherZ <84158982+KristopherZ@users.noreply.github.com>
2022-06-22 14:50:07 -0400

    shell

--- src/ICS4UProject/Game.java
+++ src/ICS4UProject/Game.java
@@ -104,6 +104,7 @@ public class Game extends AnimationTimer {
                         Double.parseDouble(values[3]), Double.parseDouble(values[4]), image);
                 e.setShellImage(shellImage);
                 e.setGravity(new Vector(0, gravityCoefficient));
+                e.setVelocity(new Vector(((Math.random()<.5)? -1:1)*100, 0));
                 enemyShellList.add(e);
             }else if(line.startsWith("4")) {
                 Image image;
